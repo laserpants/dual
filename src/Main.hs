1b61d99457b2b6365877b0b6f9fd9cbc7030174f
Johannes Hilden <hildenjohannes@gmail.com>
2018-06-03 14:01:40 +0300

    Update main

--- src/Main.hs
+++ src/Main.hs
@@ -7,5 +7,5 @@ import NumericPrelude
 
 main :: IO ()
 main = do
-    print (5 :+ 0 + 3 :+ 0 :: Dual Int)
-    print (5 :+ 0 * 3 :+ 0 :: Dual Int)
+    print (5:+0 + 3:+0 :: Dual Int)
+    print (5:+0 * 3:+0 :: Dual Int)
