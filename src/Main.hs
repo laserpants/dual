797233a7aa4fb05ee52936cf70559170b352a77d
Johannes Hilden <hildenjohannes@gmail.com>
2018-06-03 13:56:38 +0300

    Add code

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RebindableSyntax #-}
 module Main where
 
+import Math.Dual
+import NumericPrelude
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = print (5 :+ 0 + 3 :+ 0 :: Dual Int)
--- src/Math/Dual.hs
+++ src/Math/Dual.hs
@@ -1,1 +1,21 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RebindableSyntax #-}
 module Math.Dual where
+
+import Algebra.Additive
+import Algebra.Ring
+import NumericPrelude
+import NumericPrelude.Base
+
+data Dual a = a :+ a deriving (Show, Eq)
+
+infixl 7 :+
+
+instance Algebra.Ring.C a => Algebra.Additive.C (Dual a) where
+    (x :+ a) + (y :+ b) = (x + y) :+ (a + b)
+    zero                = 0 :+ 0
+    negate (x :+ a)     = negate x :+ negate a
+
+instance Algebra.Ring.C a => Algebra.Ring.C (Dual a) where
+    (x :+ a) * (y :+ b) = x*y :+ (x*b + a*y)
+    fromInteger a       = fromInteger a :+ 0
